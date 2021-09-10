55e96ed8e2ee942809ef20486f77a5c916fc471e
jffin <22579497+jffin@users.noreply.github.com>
2021-09-10 20:35:35 +0300

    some more refactoring 0.0.15

--- pkg/args/args.go
+++ pkg/args/args.go
@@ -5,11 +5,11 @@ import (
 	"github.com/jffin/have_ibeen_pwned_multi/pkg/constants"
 )
 
-func InitArgs() (*string, *string, *string) {
-	inputFile := flag.String("input", constants.INPUT_FILE_NAME, "input file with targets")
-	outputFile := flag.String("output", constants.RESULT_FILE_NAME, "output file write results to")
-	apiKey := flag.String("key", "api-key", "hibp-api-key")
+func InitArgs() (inputFile, outputFile, apiKey *string) {
+	inputFile = flag.String("input", constants.INPUT_FILE_NAME, "input file with targets")
+	outputFile = flag.String("output", constants.RESULT_FILE_NAME, "output file write results to")
+	apiKey = flag.String("key", "api-key", "hibp-api-key")
 	flag.Parse()
 
-	return inputFile, outputFile, apiKey
+	return
 }
